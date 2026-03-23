161ceaf7e4f45053296d7e694ce507faf5c4a22d
OlivierCadoz <cadoz.olivier@gmail.com>
2026-03-23 22:15:56 +0100

    refactor(test): fix counter service tests

--- src/app/domains/Pomodoro/services/counter/counter.service.spec.ts
+++ src/app/domains/Pomodoro/services/counter/counter.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { vi } from 'vitest';
 
 import { CounterService } from './counter.service';
 
@@ -8,10 +9,12 @@ describe('CounterService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CounterService);
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
     service.clearInterval();
+    vi.useRealTimers();
   });
 
   describe('When the service is created', () => {
@@ -21,61 +24,43 @@ describe('CounterService', () => {
   });
 
   describe('Given the initial count is 25:00', () => {
-    describe('When executing startAnew', () => {
+    describe('When executing startCounter', () => {
       it('Then it should update the count to 24:59 after 1 second', () => {
         const count = service['count'];
 
-        service.startAnew();
+        service.startCounter();
+        vi.advanceTimersByTime(2000);
 
-        setTimeout(() => {
-          expect(count()).toBe('24:59');
-        }, 1100);
-      });
-    });
-  });
-
-  describe('Given the count is 24:01', () => {
-    describe('When executing startAnew', () => {
-      it('Then it should update the count to 23:59 after 1 second', () => {
-        const count = service['count'];
-        count.set('24:01');
-
-        service.startAnew();
-
-        setTimeout(() => {
-          expect(count()).toBe('24:00');
-        }, 1100);
+        expect(count()).toBe('24:59');
       });
     });
   });
 
   describe('Given the count is 00:00 and it is NOT a break', () => {
-    describe('When executing startAnew', () => {
-      it('Then it should update the count to 05:00 after 1 second', () => {
+    describe('When executing setCounterInterval', () => {
+      it('Then it should update the count to 05:00', () => {
         const count = service['count'];
         count.set('00:00');
 
-        service.startAnew();
+        service.setCounterInterval();
+        vi.advanceTimersByTime(2000)
 
-        setTimeout(() => {
-          expect(count()).toBe('05:00');
-        }, 1100);
+        expect(count()).toBe('05:00');
       });
     });
   });
 
   describe('Given the count is 00:00 and it is a break', () => {
-    describe('When executing startAnew', () => {
-      it('Then it should update the count to 05:00 after 1 second', () => {
+    describe('When executing setCounterInterval', () => {
+      it('Then it should update the count to 25:00', () => {
         const count = service['count'];
         count.set('00:00');
-        service['toggleIsBreak']();
+        service['isBreak'] = true;
 
-        service.startAnew();
+        service.setCounterInterval();
+        vi.advanceTimersByTime(2000);
 
-        setTimeout(() => {
-          expect(count()).toBe('25:00');
-        }, 1100);
+        expect(count()).toBe('25:00');
       });
     });
   });
@@ -84,39 +69,46 @@ describe('CounterService', () => {
     describe('When executing freezeCounter', () => {
       it('Then it should stop the counter and keep the same count', () => {
         const count = service['count'];
-        service.startAnew();
+        vi.advanceTimersByTime(1000);
+
+        service.startCounter();
+        vi.advanceTimersByTime(2000);
         service.freezeCounter();
+        vi.advanceTimersByTime(1000);
+
+        expect(count()).toBe('24:59');
       });
     });
   });
 
   describe('Given the counter is freezed', () => {
-    describe('When executing startAnew', () => {
+    describe('When executing startCounter', () => {
       it('Then it should start the counter where it was freezed and update the count after 1 second', () => {
         const count = service['count'];
         count.set('23:47');
-        service.startAnew();
-        service.freezeCounter();
-        service.startAnew();
+        service['isFreezed'] = true;
 
-        setTimeout(() => {
-          expect(count()).toBe('23:46');
-        }, 1100);
+        service.startCounter();
+        vi.advanceTimersByTime(1000);
+        service.freezeCounter();
+        service.unFreezeCounter();
+
+        expect(count()).toBe('23:46');
       });
     });
   });
 
   describe('Given the number of pomodori is a multiple of 4', () => {
-    describe('When executing startAnew', () => {
+    describe('When executing setCounterInterval', () => {
       it('Then it should set the break time to 15:00', () => {
         const count = service['count'];
+        count.set('00:00');
         service['pomodoriNumber'] = 4;
 
-        service.startAnew();
+        service.setCounterInterval();
+        vi.advanceTimersByTime(2000)
 
-        setTimeout(() => {
-          expect(count()).toBe('15:00');
-        }, 1100);
+        expect(count()).toBe('15:00');
       });
     });
   });
